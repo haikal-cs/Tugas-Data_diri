e857a7b604cad3768cc8a301fb1f1da61273f5b4
haikal-cs <haikalmn7@gmail.com>
2025-07-25 08:17:56 +0700

    Add files via upload

---
+++ Data_diri.java
@@ -0,0 +1,18 @@
+public class Data_diri {
+  public static void main(String[] args) {
+    
+    String name = "Haechal M.N";
+    String Kelas = "XI-RPL";
+    String Nis = "01242136032";
+    String Alamat = "Ciroyom - GG.Ikhlas";
+    String Telepon = "085641851292";
+    String Gmail = "haikalmn7@gmail.com";
+
+    System.out.println(" Nama = " + name);
+    System.out.println(" Kelas = " + Kelas);
+    System.out.println(" Nis = " + Nis);
+    System.out.println(" Alamat = " + Alamat);
+    System.out.println(" Telepon = " + Telepon);
+    System.out.println(" Gmail = " + Gmail);
+  }
+}
